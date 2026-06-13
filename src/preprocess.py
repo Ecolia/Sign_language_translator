3b063d1e8164dbdb5fdcec9ea6b094cd145b658f
Honey <honey@example.com>
2026-06-13 12:56:54 +0530

    Added augmentation for reducing overfitting

--- src/preprocess.py
+++ src/preprocess.py
@@ -7,7 +7,13 @@ DATASET_PATH = "dataset/asl_alphabet_train"
 
 datagen = ImageDataGenerator(
     rescale=1.0 / 255,
-    validation_split=0.2
+    validation_split=0.2,
+    rotation_range=15,
+    width_shift_range=0.1,
+    height_shift_range=0.1,
+    zoom_range=0.1,
+    shear_range=0.1,
+    horizontal_flip=False
 )
 
 train_generator = datagen.flow_from_directory(
